38ad393d4691d46fcc78a104195c03ee44b88a36
Ahmedreda122 <ahmalolireda12@gmail.com>
2023-05-03 23:44:13 +0300

    Uploading

--- src/toffeeSystem/App.java
+++ src/toffeeSystem/App.java
@@ -1,11 +1,10 @@
 package toffeeSystem;
+
 import java.util.Scanner;
 import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.xml.validation.Validator;
-
 import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.sql.*;
 import toffeeSystem.*;
@@ -18,30 +17,33 @@ public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
         // try {
-        //     Class.forName("org.sqlite.JDBC");
-        //     Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
-        //     Statement stmt = conn.createStatement();
-        //     ResultSet set =  stmt.executeupdate(query);
-        //     **to insert data in database
-        //      String query = "insert into persons(name, email, password, phoneNum, address, type) values('ahmedReda', 'ahmalolyreda@gmail.com', 'ahmed123sd#@', '01123568974', 'ahmedredashahtabayoumi','admin')";
-        //     **print table
-        //     String query = "select * from persons";
-        //     ResultSet set =  stmt.executeQuery(query);
-        //     while (set.next()) {
-        //         System.out.println(set.getString(1) + " " + set.getString(2)
-        //                 + " " + set.getString(3));
-        //     }
+        // Class.forName("org.sqlite.JDBC");
+        // Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+        // Statement stmt = conn.createStatement();
+        // ResultSet set = stmt.executeupdate(query);
+        // **to insert data in database
+        // String query = "insert into persons(name, email, password, phoneNum, address,
+        // type) values('ahmedReda', 'ahmalolyreda@gmail.com', 'ahmed123sd#@',
+        // '01123568974', 'ahmedredashahtabayoumi','admin')";
+        // **print table
+        // String query = "select * from persons";
+        // ResultSet set = stmt.executeQuery(query);
+        // while (set.next()) {
+        // System.out.println(set.getString(1) + " " + set.getString(2)
+        // + " " + set.getString(3));
+        // }
 
         // } catch (ClassNotFoundException ex) {
-        //     Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        // Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         // }
-        
+
         // Person p = new Person();
         // System.out.println(user.verifyLogin("abdelrhmanSayed", "abdelrhamn123###"));
-        // user.signUp("abdelrhmanSayed", "abdelrhman1220@gmail.com", "abdelrhamn123###", "01017262334", "harem", "user");
-        
-        System.out.print("\033[H\033[2J");//to clean console
-        System.out.flush();//to clean console
+        // user.signUp("abdelrhmanSayed", "abdelrhman1220@gmail.com",
+        // "abdelrhamn123###", "01017262334", "harem", "user");
+
+        System.out.print("\033[H\033[2J");// to clean console
+        System.out.flush();// to clean console
         System.out.println("\t\t     <<!========== Welcome in Toffee System ==========!>>\n\n");
         System.out.print("\t(1) Registration\n");
         System.out.print("\t(2) Login\n");
@@ -51,72 +53,74 @@ public class App {
         System.out.print("<<<Enter your choice : \n>>>");
         String choice = in.nextLine();
         userAuthentication user = new userAuthentication();
-        if(choice.equals("1")){
+        if (choice.equals("1")) {
             String name, password, email, address, phoneNumber, type;
             Vaildate v = new Vaildate();
             System.out.print("<<<Enter your name : \n>>>");
             name = in.nextLine();
-            while(true){
+            while (true) {
                 System.out.print("<<<Enter your email : \n>>>");
                 email = in.nextLine();
-                if(v.checkEmail(email)){
+                if (v.checkEmail(email)) {
                     break;
-                }else{
-                    System.out.print("\t\t*** Try again.Please enter email like this form (chars + @ + (char) + . + com) ***\n");
+                } else {
+                    System.out.print(
+                            "\t\t*** Try again.Please enter email like this form (chars + @ + (char) + . + com) ***\n");
                 }
             }
-            while(true){
+            while (true) {
                 System.out.print("<<<Enter your number phone : \n>>>");
                 phoneNumber = in.nextLine();
-                if(v.checkPhoneNumber(phoneNumber)){
+                if (v.checkPhoneNumber(phoneNumber)) {
                     break;
-                }else{
-                    System.out.print("\t\t**** Please enter the mobile number formats for Egyptian mobile numbers ****\n");
+                } else {
+                    System.out.print(
+                            "\t\t**** Please enter the mobile number formats for Egyptian mobile numbers ****\n");
                 }
             }
             System.out.print("<<<Enter your Address : \n>>>");
             address = in.nextLine();
             // while(true){
-                System.out.print("<<<Enter your type(User Or Admin) : \n>>>");
-                type = in.nextLine();
-            while(true){
+            System.out.print("<<<Enter your type(User Or Admin) : \n>>>");
+            type = in.nextLine();
+            while (true) {
                 System.out.print("<<<Enter your Password : \n>>>");
                 password = in.nextLine();
-                if(v.checkPassWord(password)){
+                if (v.checkPassWord(password)) {
                     break;
-                }else{
-                    System.out.print("\t\t**** Please enter Strong password with this format(At least 7 char and At least 2 spcail char) ****\n");
+                } else {
+                    System.out.print(
+                            "\t\t**** Please enter Strong password with this format(At least 7 char and At least 2 spcail char) ****\n");
                 }
-            //     if(v.checkEmail(type)){
-            //         break;
-            //     }else{
-            //         System.out.print("\t\t**** Please enter the mobile number formats for Egyptian mobile numbers ****\n");
-            //     }
-            // }
-                
+                // if(v.checkEmail(type)){
+                // break;
+                // }else{
+                // System.out.print("\t\t**** Please enter the mobile number formats for
+                // Egyptian mobile numbers ****\n");
+                // }
+                // }
+
             }
-        user.signUp(name, email, password, phoneNumber, address, type);
-    }
-        else if(choice.equals("2")){
+            user.signUp(name, email, password, phoneNumber, address, type);
+        } else if (choice.equals("2")) {
             String name, password;
             System.out.print("<<<Enter your Username : \n>>>");
             name = in.nextLine();
             System.out.print("<<<Enter your Password : \n>>>");
             password = in.nextLine();
 
-            if(user.verifyLogin(name, password)){
+            if (user.verifyLogin(name, password)) {
                 System.out.println("you are in this system");
             }
 
-        }else{
+        } else {
             System.out.print("invalid input");
         }
         // System.out.println("");
     }
 }
 
-
-// (1) registering a user and login, 
-// (2) displaying a catalog of items loaded from somewhere, 
-// (3) shopping for items and adding them to cart, and 
+// (1) registering a user and login,
+// (2) displaying a catalog of items loaded from somewhere,
+// (3) shopping for items and adding them to cart, and
 // (4) making an order to be paid upon delivery in cash.
--- src/toffeeSystem/userAuthentication.java
+++ src/toffeeSystem/userAuthentication.java
@@ -1,13 +1,19 @@
 package toffeeSystem;
+
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
 import java.sql.Statement;
 import java.sql.DriverManager;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.*;
-public class userAuthentication{
-    userAuthentication(){
+
+public class userAuthentication {
+    userAuthentication() {
     }
-    public boolean verifyLogin(String username, String password) throws Exception{
+
+    public boolean verifyLogin(String username, String password) throws Exception {
         try {
             Class.forName("org.sqlite.JDBC");
             Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
@@ -15,7 +21,7 @@ public class userAuthentication{
             String query = "SELECT * FROM persons";
             ResultSet set = stmt.executeQuery(query);
             while (set.next()) {
-                if(set.getString(1).equals(username) && set.getString(3).equals(password)){
+                if (set.getString(1).equals(username) && set.getString(3).equals(password)) {
                     return true;
                 }
             }
@@ -24,18 +30,87 @@ public class userAuthentication{
         }
         return false;
     }
-    public void signUp(String userN, String email, String password, String phoneNum, String address, String type)throws Exception{
-         System.out.println("done " + userN + " " + email + " " + password + " " + phoneNum + "" + address + " " + type);
+
+    public void signUp(String userN, String email, String password, String phoneNum, String address, String type)
+            throws Exception {
+        System.out.println("done " + userN + " " + email + " " + password + " " + phoneNum + "" + address + " " + type);
         try {
             Class.forName("org.sqlite.JDBC");
             Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
             Statement stmt = conn.createStatement();
             // **to insert data in database
-            String query = "insert into persons(name, email, password, phoneNum, address, type) values('" + userN + "', '" + email + "', '" + password + "', '" + phoneNum + "', '" + address + "','" + type + "')";
+            String query = "insert into persons(name, email, password, phoneNum, address, type) values('" + userN
+                    + "', '" + email + "', '" + password + "', '" + phoneNum + "', '" + address + "','" + type + "')";
             stmt.executeUpdate(query);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
+    public static void sendOTP(String email) {
+        // Generate a random OTP
+        String otp = generateOTP();
+
+        // Recipient's email ID
+        String to = email;
+
+        // Sender's email ID and password
+        String from = "loginapplication12@gmail.com";
+        String password = "gxsptjnhcoutosvb";
+
+        // SMTP server details
+        String host = "smtp.gmail.com";
+
+        // Creating properties object
+        Properties props = new Properties();
+
+        // SMTP server configuration
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "587"); // 587
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        // Creating a Session object
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+            // Creating a Message object
+            Message message = new MimeMessage(session);
+
+            // Set From field
+            message.setFrom(new InternetAddress(from));
+
+            // Set To field
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+            // Set email subject
+            message.setSubject("Your OTP for login");
+
+            // Set email body
+            message.setText("Your OTP is " + otp);
+
+            // Send message
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Method to generate a random OTP
+    private static String generateOTP() {
+        int otpLength = 6;
+        String numbers = "0123456789";
+        Random random = new Random();
+        char[] otp = new char[otpLength];
+        for (int i = 0; i < otpLength; i++) {
+            otp[i] = numbers.charAt(random.nextInt(numbers.length()));
+        }
+        return new String(otp);
+    }
 }
